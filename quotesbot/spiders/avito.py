d2c4126adf69b8d04719ed744948f07ce32cb643
ayoubbenlemlih <37865694+ayoubbenlemlih@users.noreply.github.com>
2018-03-28 11:33:12 +0100

    Update avito.py

--- quotesbot/spiders/avito.py
+++ quotesbot/spiders/avito.py
@@ -14,7 +14,15 @@ class LoginSpider(scrapy.Spider):
     def after_login(self, response):
         # check login succeed before going on
         if "Changez cela aujourd" not in response.body:
-            self.logger.error("Login failed")
+            self.logger.info("Login failed")
             return
         self.logger.info("Login success")
+        return scrapy.Request(url = "https://www.avito.ma/fr/account/deactivated_ads",callback=self.deactivated)
+
+    def deactivated(self, response):
+        # check login succeed before going on
+        if "Mercedes" not in response.body:
+            self.logger.info("deactivated failed")
+            return
+        self.logger.info("deactivated success")
         return
