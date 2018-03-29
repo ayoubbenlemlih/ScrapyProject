efc896607a6d864217261bcfd29a5a14eef7bd0a
ayoubbenlemlih <37865694+ayoubbenlemlih@users.noreply.github.com>
2018-03-29 13:51:02 +0100

    Update avito.py

--- quotesbot/spiders/avito.py
+++ quotesbot/spiders/avito.py
@@ -82,7 +82,7 @@ class LoginSpider(scrapy.Spider):
         Content-Disposition: form-data; name="chosenVAS"
         ------WebKitFormBoundary5M31VB0KGabpTJF8--
         '''
-        return scrapy.FormRequest(url="https://www2.avito.ma/ai/create/0",body=body,headers={'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7', 'cache-control': 'max-age=0', 'content-length': '2934', 'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary5M31VB0KGabpTJF8',  'origin': 'https://www2.avito.ma', 'referer': 'https://www2.avito.ma/ai/form/0', 'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'},
+        return scrapy.FormRequest(url="https://www2.avito.ma/ai/create/0",body=body,headers={'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7', 'cache-control': 'max-age=0', 'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary5M31VB0KGabpTJF8',  'origin': 'https://www2.avito.ma', 'referer': 'https://www2.avito.ma/ai/form/0', 'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'},
                     callback=self.after_login)
         
 
