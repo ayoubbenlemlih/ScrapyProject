23476d5afa4dc0c428a17a2182f367a996e0fb41
ayoubbenlemlih <37865694+ayoubbenlemlih@users.noreply.github.com>
2018-03-29 10:05:24 +0100

    Update avito.py

--- quotesbot/spiders/avito.py
+++ quotesbot/spiders/avito.py
@@ -2,147 +2,132 @@ import scrapy
 
 class LoginSpider(scrapy.Spider):
     name = 'avito'
-    start_urls = ['https://www.avito.ma/account/login']
+    start_urls = ['https://www2.avito.ma/ai/form/0']
 
     def parse(self, response):
-        return scrapy.FormRequest.from_response(
-            response,
-            formdata={'email': 'ayoub.benlemlih@gmail.com', 'passwd': 'youssef2010', 'login': 'Se connecter'},
-            callback=self.after_login
-        )
-
-    def after_login(self, response):
-        # check login succeed before going on
-        if "Nabil" not in response.body:
-            self.logger.info("Login failed")
-            return
-        self.logger.info("Login success")
-        body = '''------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        body = '''------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="category_group"
 
         7010
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="type"
 
         s
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="lang"
 
         fr
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="rooms"
 
 
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="beds"
 
 
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="size"
 
 
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="brand"
 
 
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="model"
 
 
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="fuel"
 
 
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="regdate"
 
 
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="mileage"
 
 
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="pfiscale"
 
 
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="region"
 
-        7
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        13
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="area"
 
-        566
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        1076
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="address"
 
 
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="subject"
 
-        eeeeeeeeeeeeeee
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        vvvvvvvvv
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="body"
 
-        aoaoaoaoaoaoaoaoaoaoaoaoa
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        aaaaaaaaaaaaaaaaaaaaddddddddddddd
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="mandatory_cv"
 
         0
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="price"
 
-        800 000
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        100 000
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="price_meter"
 
         0
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="images[]"; filename=""
         Content-Type: application/octet-stream
 
 
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="company_ad"
 
         0
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="name"
 
         Nabil
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="email"
 
         ayoub.benlemlih@gmail.com
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="phone"
 
         0615016786
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
-        Content-Disposition: form-data; name="email_confirm"
-
-        ayoub.benlemlih@gmail.com
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="passwd"
 
-
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        youssef2010
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="account_type"
 
         0
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="chosenVasId"
 
 
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP
+        ------WebKitFormBoundary5M31VB0KGabpTJF8
         Content-Disposition: form-data; name="chosenVAS"
 
 
-        ------WebKitFormBoundaryvK2CBXmGBBLqBPsP--
+        ------WebKitFormBoundary5M31VB0KGabpTJF8--
         '''
-        return scrapy.FormRequest(url="https://www2.avito.ma/ai/create/0",body=body,headers={'Content-Type':'multipart/form-data; boundary=----WebKitFormBoundaryvK2CBXmGBBLqBPsP'},
-                    callback=self.form,dont_filter=True)
+        return scrapy.FormRequest(url="https://www2.avito.ma/ai/create/0",body=body,headers={'Content-Type':'multipart/form-data; boundary=----WebKitFormBoundary5M31VB0KGabpTJF8'},
+                    callback=self.after_login)
+        
 
-    def form(self, response):
+    def after_login(self, response):
         self.logger.info(response.body)
         return
