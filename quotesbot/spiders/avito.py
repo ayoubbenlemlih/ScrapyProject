e2ca3a77fe305eca24f1f2da0797bf61790d3aea
ayoubbenlemlih <37865694+ayoubbenlemlih@users.noreply.github.com>
2018-03-30 09:54:15 +0100

    Update avito.py

--- quotesbot/spiders/avito.py
+++ quotesbot/spiders/avito.py
@@ -97,10 +97,11 @@ class LoginSpider(scrapy.Spider):
         Content-Disposition: form-data; name="chosenVAS"
         ------WebKitFormBoundaryvK2CBXmGBBLqBPsP--
         '''
-        return scrapy.FormRequest(url="https://www2.avito.ma/ai/create/0",body=body,headers={'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7', 'cache-control': 'max-age=0', 'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary5M31VB0KGabpTJF8',  'origin': 'https://www2.avito.ma', 'referer': 'https://www2.avito.ma/ai/form/0', 'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'},
+        return scrapy.FormRequest(url="https://www2.avito.ma/ai/create/0",body=body,
                      callback=self.form)
 
     def form(self, response):
+        self.logger.info(response.body)
         self.logger.info("success form")
         self.logger.info("headers form"+str(response.request.headers))
         return
