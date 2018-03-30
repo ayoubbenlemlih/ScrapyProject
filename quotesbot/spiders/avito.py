6ee647b83f25f60e6300c80ea8b517c9ac268d3c
ayoubbenlemlih <37865694+ayoubbenlemlih@users.noreply.github.com>
2018-03-30 11:54:59 +0100

    Update avito.py

--- quotesbot/spiders/avito.py
+++ quotesbot/spiders/avito.py
@@ -18,7 +18,7 @@ class LoginSpider(scrapy.Spider):
         self.logger.info("Login success")
         self.logger.info("headers login"+str(response.request.headers))
         self.logger.info("body login"+str(response.request.body))
-        return scrapy.Request (url = "https://www2.avito.ma/ai/form/0" ,headers={'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7', 'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'},
+        return scrapy.Request (url = "https://www2.avito.ma/ai/form/0" ,headers={'content-length': '3079','accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7', 'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'},
  callback = self.form,dont_filter=True)
 
     def form(self, response):
