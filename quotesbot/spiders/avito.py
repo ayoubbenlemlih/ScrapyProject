5b15147a65d97f9e240a17773fe5042447eaceb8
ayoubbenlemlih <37865694+ayoubbenlemlih@users.noreply.github.com>
2018-04-03 14:54:22 +0100

    Update avito.py

--- quotesbot/spiders/avito.py
+++ quotesbot/spiders/avito.py
@@ -157,7 +157,7 @@ Content-Disposition: form-data; name="chosenVAS"
         r = requests.post(url, files=files, data=payload)
         self.logger.info("1"+r.text)
         no_file_multipart_req = requests.Request('POST', 'http://httpbin.org/anything' ,files={'name2': (None, 'content')}).prepare()
-        print("2",no_file_multipart_req.body.decode('utf-8'))
+         self.logger.info("2"+no_file_multipart_req.body.decode('utf-8'))
         return scrapy.Request(url="https://www2.avito.ma/ai/create/0",method = 'POST',body = body,headers={'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7', 'cache-control': 'max-age=0', 'content-type': 'multipart/form-data; boundary=------WebKitFormBoundary5tKbPFdKTvzi2zTy',  'origin': 'https://www2.avito.ma', 'referer': 'https://www2.avito.ma/ai/form/0', 'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'},
                                   callback=self.create,dont_filter=True)
 
