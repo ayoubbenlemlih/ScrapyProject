e40650b6a619199ce6d649f47f242531ee54c54f
ayoubbenlemlih <37865694+ayoubbenlemlih@users.noreply.github.com>
2018-04-04 11:58:03 +0100

    Update avito.py

--- quotesbot/spiders/avito.py
+++ quotesbot/spiders/avito.py
@@ -10,8 +10,8 @@ class LoginSpider(scrapy.Spider):
     def parse(self, response):
 
         url = 'https://www.avito.ma/account/login'
-        data = {'email': 'ayoub.benlemlih@gmail.com'}
-        r=requests.post('https://www.avito.ma/account/do_login',data = data }
+        data = {'email': 'ayoub.benlemlih@gmail.com', 'passwd': 'youssef2010', 'login': 'Se connecter'}
+        r=requests.post('https://www.avito.ma/account/do_login',data = data ,allow_redirects )
         self.logger.info(r.text)
         return
     
