534a9775d7d96a0a0b9653b6588329df61810516
ayoubbenlemlih <37865694+ayoubbenlemlih@users.noreply.github.com>
2018-04-04 11:59:00 +0100

    Update avito.py

--- quotesbot/spiders/avito.py
+++ quotesbot/spiders/avito.py
@@ -11,7 +11,7 @@ class LoginSpider(scrapy.Spider):
 
         url = 'https://www.avito.ma/account/login'
         data = {'email': 'ayoub.benlemlih@gmail.com', 'passwd': 'youssef2010', 'login': 'Se connecter'}
-        r=requests.post('https://www.avito.ma/account/do_login',data = data ,allow_redirects )
+        r=requests.post('https://www.avito.ma/account/do_login',data = data ,allow_redirects=True )
         self.logger.info(r.text)
         return
     
