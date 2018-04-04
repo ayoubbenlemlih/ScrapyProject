c6be650cc67d3f6e8b436e7f079b952c0affcef2
ayoubbenlemlih <37865694+ayoubbenlemlih@users.noreply.github.com>
2018-04-04 14:29:59 +0100

    Update avito.py

--- quotesbot/spiders/avito.py
+++ quotesbot/spiders/avito.py
@@ -14,9 +14,9 @@ class LoginSpider(scrapy.Spider):
         cookies =rsp.cookies
         self.logger.info(cookies)
         data = {'email': 'ayoub.benlemlih@gmail.com', 'passwd': 'youssef2010', 'login': 'Se connecter'}
-        r=requests.post('https://www.avito.ma/account/do_login',cookies=cookies ,data = data ,allow_redirects=True )
+        r=requests.post('https://www.avito.ma/account/do_login',cookies=cookies ,data = data ,allow_redirects=False )
         self.logger.info(r.text)
-        cookies=cookies.update(r.cookies)
+        cookies.update(r.cookies)
         self.logger.info(cookies)
         r2=requests.get('https://www2.avito.ma/ai/form/0',cookies=cookies)
         self.logger.info(r2.text)       
