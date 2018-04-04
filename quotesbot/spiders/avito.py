9c33accc286624f37ca2e31545e275fee9f3bad8
ayoubbenlemlih <37865694+ayoubbenlemlih@users.noreply.github.com>
2018-04-04 14:37:02 +0100

    Update avito.py

--- quotesbot/spiders/avito.py
+++ quotesbot/spiders/avito.py
@@ -14,12 +14,33 @@ class LoginSpider(scrapy.Spider):
         cookies =rsp.cookies
         self.logger.info(cookies)
         data = {'email': 'ayoub.benlemlih@gmail.com', 'passwd': 'youssef2010', 'login': 'Se connecter'}
-        r=requests.post('https://www.avito.ma/account/do_login',cookies=cookies ,data = data ,allow_redirects=False )
+        r=requests.post('https://www.avito.ma/account/do_login',cookies=cookies ,data = data ,allow_redirects=True )
         self.logger.info(r.text)
         cookies.update(r.cookies)
         self.logger.info(cookies)
         r2=requests.get('https://www2.avito.ma/ai/form/0',cookies=cookies)
-        self.logger.info(r2.text)       
+        self.logger.info(r2.text)
+        cookies.update(r2.cookies)
+        
+        headers={'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
+                 'cache-control': 'max-age=0','origin': 'https://www2.avito.ma', 'referer': 'https://www2.avito.ma/ai/form/0', 'upgrade-insecure-requests': '1',
+                 'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
+        files = {'category_group': (None, '7010'),'type': (None, 's'),'lang': (None, 'fr'),'rooms': (None, '')
+                ,'beds': (None, ''),'size': (None, ''),'brand': (None, ''),'model': (None, '')
+                ,'fuel': (None, ''),'regdate': (None, ''),'mileage': (None, ''),'pfiscale': (None, '')
+                ,'region': (None, '17'),'area': (None, '154'),'address': (None, ''),'subject': (None, 'ffffffffffffffffffffffffffffffffffffff')
+                ,'body': (None, 'dddddddddddddddddddddddddddddddddddd'),'mandatory_cv': (None, '0'),'price': (None, '100 000')
+                 ,'price_meter': (None, '0'),'images[]': ('', '','application/octet-stream'),'company_ad': (None, '0')
+                ,'name': (None, 'Nabil'),'email': (None, 'ayoub.benlemlih@gmail.com'),'phone': (None, '0615016786')
+                ,'email_confirm': (None, 'ayoub.benlemlih@gmail.com'),'passwd': (None, ''),'account_type': (None, '0'),'chosenVasId': (None, '')
+                ,'chosenVAS': (None, '')}
+        url = 'https://www2.avito.ma/ai/create/0'
+        no_file_multipart_req = requests.Request('POST', url ,files=files,headers=headers).prepare()
+        self.logger.info(no_file_multipart_req.body.decode('utf-8'))
+        s = requests.Session()
+        r=s.send(no_file_multipart_req,allow_redirects= True)
+        self.logger.info(r.text)
         return
     
     
