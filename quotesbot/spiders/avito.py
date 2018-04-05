fbb84ab72dba7a17669bd5b7fa381e63833027b3
ayoubbenlemlih <37865694+ayoubbenlemlih@users.noreply.github.com>
2018-04-05 16:43:02 +0100

    Update avito.py

--- quotesbot/spiders/avito.py
+++ quotesbot/spiders/avito.py
@@ -1,50 +1,158 @@
-
-
 import scrapy
-import requests
 
 class LoginSpider(scrapy.Spider):
     name = 'avito'
-    start_urls = ['https://www.avito.ma']
+    start_urls = ['https://www.avito.ma/account/login']
 
     def parse(self, response):
+        return scrapy.FormRequest.from_response(
+            response,
+            formdata={'email': 'ayoub.benlemlih@gmail.com', 'passwd': 'youssef2010', 'login': 'Se connecter'},callback=self.after_login
+        )
+    
+    def after_login(self, response):
+        # check login succeed before going on
+        if "Nabil" not in response.body:
+            self.logger.info("Login failed")
+            return
+        self.logger.info("Login success")
+        self.logger.info("headers login"+str(response.request.headers))
+        self.logger.info("body login"+str(response.request.body))
+        return scrapy.Request (url = "https://www2.avito.ma/ai/form/0" ,headers={'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7', 'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'},
+ callback = self.form,dont_filter=True)
 
-        url = 'https://www.avito.ma/account/login'
-        rsp = requests.get(url)
-        cookies =rsp.cookies
-        self.logger.info(cookies)
-        data = {'email': 'a.benlemlih@mgpap.org.ma', 'passwd': 'youssef2010', 'login': 'Se connecter'}
-        r=requests.post('https://www.avito.ma/account/do_login',cookies=cookies ,data = data ,allow_redirects=False )
-        self.logger.info(r.text)
-        cookies.update(r.cookies)
-        self.logger.info(cookies)
-        r2=requests.get('https://www2.avito.ma/ai/form/0',cookies=cookies)
-        self.logger.info(r2.text)
-        cookies.update(r2.cookies)
-        self.logger.info(cookies)
+    def form(self, response):
+        self.logger.info("headers form"+str(response.request.headers))
+        self.logger.info("body form"+str(response.request.body))
         
-        headers={'authority':'www2.avito.ma','method':'POST','path':'/ai/create/0','scheme':'https','accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
-                 'cache-control': 'max-age=0','origin': 'https://www2.avito.ma', 'referer': 'https://www2.avito.ma/ai/form/0', 'upgrade-insecure-requests': '1',
-                 'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
-        files = {'category_group': (None, '7010'),'type': (None, 's'),'lang': (None, 'fr'),'rooms': (None, '')
-                ,'beds': (None, ''),'size': (None, ''),'brand': (None, ''),'model': (None, '')
-                ,'fuel': (None, ''),'regdate': (None, ''),'mileage': (None, ''),'pfiscale': (None, '')
-                ,'region': (None, '17'),'area': (None, '154'),'address': (None, ''),'subject': (None, 'wwwwwwwwwwwwwwwwwwwwwwwwwwww')
-                ,'body': (None, 'dddddddddddddddddddddddddddddddddddd'),'mandatory_cv': (None, '0'),'price': (None, '100 000')
-                 ,'price_meter': (None, '0'),'images_urls[]': (None, '2665577777.jpg'),'images[]': ('', '','application/octet-stream'),'company_ad': (None, '0')
-                ,'name': (None, 'Ayoub'),'email': (None, 'a.benlemlih@mgpap.org.ma'),'phone': (None, '0615016786')
-                ,'email_confirm': (None, 'a.benlemlih@mgpap.org.ma'),'passwd': (None, 'youssef2010'),'account_type': (None, '0'),'chosenVasId': (None, '62')
-                ,'chosenVAS': (None, 'insertion'),'validate': (None, 'Déposez votre annonce »') }
-        url = 'https://www2.avito.ma/ai/create/0'
-        url2 = 'http://httpbin.org/anything'
-        no_file_multipart_req = requests.Request('POST', url ,files=files,cookies=cookies,headers=headers).prepare()
-        self.logger.info(no_file_multipart_req.body.decode('utf-8'))
-        s = requests.Session()
-        r=s.send(no_file_multipart_req,allow_redirects= True)
-        cookies.update(r.cookies)
-        self.logger.info(cookies)
-        self.logger.info(r.text)
+        body = '''------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="category_group" 
+ 
+7010 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="type" 
+ 
+s 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="lang" 
+ 
+fr 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="rooms" 
+ 
+ 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="beds" 
+ 
+ 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="size" 
+ 
+ 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="brand" 
+ 
+ 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="model" 
+ 
+ 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="fuel" 
+ 
+ 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="regdate" 
+ 
+ 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="mileage" 
+ 
+ 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="pfiscale" 
+ 
+ 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="region" 
+ 
+17 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="area" 
+ 
+154 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="address" 
+ 
+ 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="subject" 
+ 
+fffffffffffffffffffffffffffffffffffffo 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="body" 
+ 
+dddddddddddddddddddddddddddddddddddd 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="mandatory_cv" 
+ 
+0 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="price" 
+ 
+100 000 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="price_meter" 
+ 
+0 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="images[]"; filename="" 
+Content-Type: application/octet-stream 
+ 
+ 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="company_ad" 
+ 
+0 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="name" 
+ 
+Nabil 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="email" 
+ 
+ayoub.benlemlih@gmail.com 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="phone" 
+ 
+0615016786 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="email_confirm" 
+ 
+ayoub.benlemlih@gmail.com 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="passwd" 
+ 
+ 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="account_type" 
+ 
+0 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="chosenVasId" 
+ 
+62 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy 
+Content-Disposition: form-data; name="chosenVAS" 
+ 
+insertion 
+------WebKitFormBoundary5tKbPFdKTvzi2zTy-- 
+'''
+        return scrapy.Request(url="https://www2.avito.ma/ai/create/0",method = 'POST',body = body,headers={'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7', 'cache-control': 'max-age=0', 'content-type': 'multipart/form-data; boundary=------WebKitFormBoundary5tKbPFdKTvzi2zTy',  'origin': 'https://www2.avito.ma', 'referer': 'https://www2.avito.ma/ai/form/0', 'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'},
+                                  callback=self.create,dont_filter=True)
+
+    def create(self, response):
+        self.logger.info(response.body)
+        self.logger.info("headers create"+str(response.request.headers))
+        self.logger.info("body create"+str(response.request.body))        
         return
-    
-    
