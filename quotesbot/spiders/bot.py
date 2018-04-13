f3e1b5a84cc82062638d95fcad0e056729e18e0b
ayoubbenlemlih <37865694+ayoubbenlemlih@users.noreply.github.com>
2018-04-13 09:27:36 +0100

    Update bot.py

--- quotesbot/spiders/bot.py
+++ quotesbot/spiders/bot.py
@@ -25,13 +25,13 @@ class LoginSpider(scrapy.Spider):
                  'accept-encoding': 'gzip, deflate, br', 'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
                  'cache-control': 'max-age=0','origin': 'https://www2.avito.ma', 'referer': 'https://www2.avito.ma/ai/form/0', 'upgrade-insecure-requests': '1',
                  'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
-      #  img =  urllib.request.urlopen('https://www.avito.ma/thumbs/26/2665577777.jpg ')
+      #  img =  urllib.request.urlopen('https://www.avito.ma/thumbs/26/2665577777.jpg')
         
         files = {'category_group': (None, '7010'),'type': (None, 's'),'lang': (None, 'fr'),'rooms': (None, '')
                 ,'beds': (None, ''),'size': (None, ''),'brand': (None, ''),'model': (None, '')
                 ,'fuel': (None, ''),'regdate': (None, ''),'mileage': (None, ''),'pfiscale': (None, '')
                 ,'region': (None, '17'),'area': (None, '154'),'address': (None, ''),'subject': (None, 'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeesept')
-                ,'body': (None, 'llllllllllllllllllllllllsept'),'mandatory_cv': (None, '0'),'price': (None, '100 000')
+                ,'body': (None, '•	Dzazzzzzzzzzzzz \n •	zaddzzzzzzz'),'mandatory_cv': (None, '0'),'price': (None, '100 000')
                  ,'price_meter': (None, '0'),'images_urls[]': (None, '2665577777.jpg'),'images[]': ('', '','application/octet-stream'),'company_ad': (None, '0')
                 ,'name': (None, 'Ayoub'),'email': (None, 'a.benlemlih@mgpap.org.ma'),'phone': (None, '0615016786')
                 ,'email_confirm': (None, 'a.benlemlih@mgpap.org.ma'),'passwd': (None, ''),'account_type': (None, '0'),'chosenVasId': (None, '62')
